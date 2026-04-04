dc7d6ad4f367e51ad71502402ceb6bacb04dadc3
Hamza Bazaz <hamza.bazaz7@gmail.com>
2026-04-04 15:05:17 +0530

    backend ready for deployment

--- backend/news2Post/news2Post/src/main/java/com/hamza/news2post/config/WebConfig.java
+++ backend/news2Post/news2Post/src/main/java/com/hamza/news2post/config/WebConfig.java
@@ -5,14 +5,13 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer{
-	  @Override
-	    public void addCorsMappings(CorsRegistry registry) {
-	        registry.addMapping("/**")
-	                .allowedOrigins("http://localhost:4200")
-	                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-	                .allowedHeaders("*")
-	                .allowCredentials(true);
-	    }
-
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")  // allow all (quick fix)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
 }
+
