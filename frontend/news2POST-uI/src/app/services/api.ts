eaa957b00ac4620b460252088ef3f9e61be7ba3f
Hamza Bazaz <hamza.bazaz7@gmail.com>
2026-04-05 15:07:24 +0530

    use environment config

--- frontend/news2POST-uI/src/app/services/api.ts
+++ frontend/news2POST-uI/src/app/services/api.ts
@@ -4,19 +4,21 @@ import { Observable } from 'rxjs';
 import { Article } from '../models/article.model';
 import { GenerateRequest } from '../models/generate-request.model';
 import { GenerateResponse } from '../models/generate-response.model';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Api {
-   private baseUrl = 'https://trendcraft-ai.onrender.com';
+
+  private baseUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
   fetchArticles(topic: string): Observable<{ topic: string; articles: Article[] }> {
     const params = new HttpParams().set('topic', topic);
     return this.http.get<{ topic: string; articles: Article[] }>(
-      `${this.baseUrl}/articles?topic=${topic}`,
+      `${this.baseUrl}/articles`,
       { params }
     );
   }
@@ -27,5 +29,4 @@ export class Api {
       payload
     );
   }
-  
 }
---
+++ frontend/news2POST-uI/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://trendcraft-ai.onrender.com'
+};
